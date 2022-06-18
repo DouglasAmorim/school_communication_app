3f389937fe581ef60bd86bccd20a7eb504400635
DouglasAmorim <amorimdos@gmail.com>
2022-06-18 16:45:21 -0300

    removido implementações antigas não utilizadas

--- flutter_app/lib/models/ApiImplementations/ApiImpl.dart
+++ flutter_app/lib/models/ApiImplementations/ApiImpl.dart
@@ -5,7 +5,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:tcc_ifsc/models/EstruturaMensagem.dart';
-import 'package:tcc_ifsc/models/Storage/SecureStorage.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:tcc_ifsc/Auth/secrets.dart';
 
